ea621db6f415c80c1c5defe4ab95e85c9088a4a7
Tien Tran <tientran2290@gmail.com>
2022-05-22 19:29:02 -0700

    front-end/create_page_layout

--- frontend/src/components/Nav/NavSearchBar.jsx
+++ frontend/src/components/Nav/NavSearchBar.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function NavSearchBar() {
   return (
-    <div>NavSearchBar</div>
+    <div className='flex mr-auto items-center'>
+      <input type="text" placeholder='Search moves' />
+      <div>
+        <button className='bg-indigo-600 text-white text-sm leading-6 font-medium py-2 px-3 rounded-lg' type='button'>Search</button>
+      </div>
+  </div>
   )
 }
