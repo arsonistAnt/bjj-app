3cdb3043e7495a068d26b86f4e96a9e6eb2e850a
Tien <tn-tran@users.noreply.github.com>
2022-05-23 17:22:09 +0000

    front-end/create-page-layout (#31)
    * front-end/add_tailwind_package
    
    * front-end/create_page_layout

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,6 +3,13 @@ import Nav from './Nav/Nav'
 
 export default function Home() {
   return (
-    <Nav/>
+    <div>
+      <Nav/>
+      <main className='w-full h-screen flex justify-center bg-slate-100'>
+        <div className="w-7/12 flex justify-center bg-slate-300 border-x-2 border-x-slate-700">
+          Main
+        </div>
+      </main>
+    </div>
   )
 }
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -3,12 +3,14 @@ import NavSearchBar from './NavSearchBar'
 
 export default function Nav() {
   return (
-    <div>
-      <div>
-        <img src="" alt="Logo" srcset="" />
-        <div>Title</div>
+    <nav className='w-full flex justify-center  py-8 bg-cyan-50'>
+      <div className='w-7/12 flex justify-center bg-teal-200'>
+        <div className='flex justify-center items-center mr-auto px-12 bg-yellow-300 '>
+          <img src="" alt="Logo" srcset="" />
+          <div>Title</div>
+        </div>
+        <NavSearchBar/>
       </div>
-      <NavSearchBar/>
-    </div>
+    </nav>
   )
 }
