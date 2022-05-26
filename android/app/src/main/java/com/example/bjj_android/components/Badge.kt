466629da6bf53778a60a7c886887aad9e3cfebab
Anthony <arsonistant@gmail.com>
2022-05-25 19:20:07 -0700

    Apply ktlint

--- android/app/src/main/java/com/example/bjj_android/components/Badge.kt
+++ android/app/src/main/java/com/example/bjj_android/components/Badge.kt
@@ -1,13 +1,9 @@
 package com.example.bjj_android.components
 
-import android.graphics.drawable.Drawable
 import android.graphics.drawable.Icon
-import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
@@ -17,23 +13,20 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.contentValuesOf
 import com.example.bjj_android.R
 
 @Composable
 fun Badge(
-    text : String,
-    @ColorRes color : Int,
-    onClick : () -> Unit = {},
-    @DrawableRes icon : Int? = null){
+    text: String,
+    @ColorRes color: Int,
+    onClick: () -> Unit = {},
+    @DrawableRes icon: Int? = null
+) {
     // Primary color that has been passed into the component constructor.
     val primaryColor = colorResource(id = color)
 
@@ -42,18 +35,19 @@ fun Badge(
         border = BorderStroke(width = 1.dp, color = primaryColor),
         elevation = 8.dp,
     ) {
-        Row(modifier = Modifier
-            .padding(4.dp)
-            .clickable { onClick() }) {
+        Row(
+            modifier = Modifier
+                .padding(4.dp)
+                .clickable { onClick() }
+        ) {
             // Display icon if a bitmap is provided.
             icon?.let {
                 val iconPainter = painterResource(id = it)
-                androidx.compose.material.Icon(painter = iconPainter, contentDescription = "Badge Icon $it" )
+                androidx.compose.material.Icon(painter = iconPainter, contentDescription = "Badge Icon $it")
             }
             Text(text = text, color = primaryColor)
         }
     }
-
 }
 
 /**
@@ -61,6 +55,6 @@ fun Badge(
  */
 @Preview
 @Composable
-private fun BadgePreview(){
-    Badge(text = "Testing", color = R.color.teal_200 )
+private fun BadgePreview() {
+    Badge(text = "Testing", color = R.color.teal_200)
 }
